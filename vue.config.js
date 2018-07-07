6d95694de36473999efc52e6fdd6de8d7bf4c33b
xuwenchao <842390367@qq.com>
2018-07-07 18:40:28 +0800

    chore: remove eatract css

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
 module.exports = {
+  css: {
+    extract: false
+  },
   configureWebpack: {
     plugins: [
       // 页面预渲染
